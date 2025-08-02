608dc27cc2194563d3aaeb14848c6b73943c39c2
Vaishali Goyal <vishug654@gmail.com>
2025-08-02 17:58:36 +0530

    Remove snipmate-frontend-temp folder

--- snipmate-frontend-temp/src/components/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { isAuthenticated } from "../utils/auth";
-
-const PrivateRoute = ({ children }) => {
-  return isAuthenticated() ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
